8d8b1dc160d2d4f3af5ed432efe86853e5681752
Rogerio Silva <roger.alves.silva84@gmail.com>
2024-10-09 17:39:28 -0300

    Excluindo um produto com POST

--- excluir-produto.php
+++ excluir-produto.php
@@ -6,4 +6,6 @@ require_once __DIR__ . "/src/Repositorio/ProdutoRepositorio.php";
 
 $produtoRepositorio = new ProdutoRepositorio($pdo);
 
-$produtoRepositorio->deletar($_GET['id']);
+$produtoRepositorio->deletar($_POST['id']);
+
+header("Location: admin.php");
