115e70827606207f1b2729c27978aa68ac5cd358
Rogerio Silva <roger.alves.silva84@gmail.com>
2024-10-11 17:15:59 -0300

    Resolução do exercício

--- src/Repositorio/ProdutoRepositorio.php
+++ src/Repositorio/ProdutoRepositorio.php
@@ -77,4 +77,28 @@ class ProdutoRepositorio
         $statement->bindValue(5, $produto->getImagem());
         $statement->execute();
     }
+
+    public function buscar(int $id)
+    {
+        $sql =  "SELECT * FROM produtos WHERE id = ?";
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(1, $id);
+        $statement->execute();
+
+        $dados = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return  $this->formarObjeto($dados);
+    }
+
+    public function atualizar($produto) {
+        $sql = "UPDATE produtos SET tipo = ?, nome = ?, descricao = ?, preco = ?, imagem = ? WHERE id = ?";
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(1, $produto->getTipo());
+        $statement->bindValue(2, $produto->getNome());
+        $statement->bindValue(3, $produto->getDescricao());
+        $statement->bindValue(4, $produto->getPreco());
+        $statement->bindValue(5, $produto->getImagem());
+        $statement->bindValue(6, $produto->getId());
+        $statement->execute();
+    }
 } 
